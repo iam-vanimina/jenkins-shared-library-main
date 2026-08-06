2c4b0b7af523f6dd27e514c3780ea667e7880417
iam-vanimina <venkataramcloud@gmail.com>
2026-08-06 14:38:49 +0530

    Update nodeJSEKSPipeline.groovy

--- vars/nodeJSEKSPipeline.groovy
+++ vars/nodeJSEKSPipeline.groovy
@@ -68,10 +68,14 @@ def call(Map configMap){
                     // Automatically uses the EC2 IAM Role to generate a login password token
                         sh """
                         #aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
+                        #docker build -t  ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${project}/${component}:${appVersion} .
+                        #docker push ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${project}/${component}:${appVersion}
+                        
                         aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
-                        docker build -t  ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${project}/${component}:${appVersion} .
+                                            
+                        docker build -t  ${ACC_ID}.dkr.ecr.ap-south-1.amazonaws.com/${project}/${component}:${appVersion} .
 
-                        docker push ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${project}/${component}:${appVersion}
+                        docker push ${ACC_ID}.dkr.ecr.ap-south-1.amazonaws.com/${project}/${component}:${appVersion}
                         """
                    // }
                     
